69104d2321277f55c4f8b6a15926188d3d7b9f2f
hoja2242 <hoja2242@naver.com>
2020-10-05 15:54:45 +0900

    프로젝트생성

---
+++ bit_001/src/main/java/com/example/demo/Bit001Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bit001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bit001Application.class, args);
+	}
+
+}
